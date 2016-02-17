2ac1f339e6a477c0a827a8def20d551e214dcb03
Juanfran Correas <jfcorreas@gmail.com>
2016-02-17 23:40:24 +0100

    Empty Users module added

---
+++ app/components/users/users.module.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name users
+ * @description
+ * # users
+ *
+ * Users Management module of the application
+ */
+
+angular
+  .module('baseAngular.users', [
+    'baseAngular.core'
+  ]);
