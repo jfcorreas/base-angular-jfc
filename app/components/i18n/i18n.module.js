cf3ef79e131408cb579e09baa56e1e670b8b715c
Juanfran Correas <jfcorreas@gmail.com>
2016-02-21 23:59:20 +0100

    Refactorin config & constants of i18n module

--- app/components/i18n/i18n.module.js
+++ app/components/i18n/i18n.module.js
@@ -14,4 +14,22 @@ angular
     'baseAngular.core',
     'pascalprecht.translate',
     'tmh.dynamicLocale'
-  ]);
+  ])
+  .constant('LOCALES', {
+    'locales': {
+        'es_ES': 'Español',
+        'en_US': 'English'
+    },
+    'preferredLocale': 'es_ES'
+  })
+  .config(['$translateProvider', 'tmhDynamicLocaleProvider', 'LOCALES',
+    function ($translateProvider, tmhDynamicLocaleProvider, LOCALES) {
+      $translateProvider.useStaticFilesLoader({
+        prefix: 'app/resources/locale-',
+        suffix: '.json'
+      })
+        .preferredLanguage(LOCALES.preferredLocale)
+        .useSanitizeValueStrategy('sanitize')
+        .useMissingTranslationHandlerLog();
+      tmhDynamicLocaleProvider.localeLocationPattern("bower_components/angular-i18n/angular-locale_{{locale}}.js");
+    }]);
--- app/components/i18n/i18n.service.js
+++ app/components/i18n/i18n.service.js
@@ -10,24 +10,6 @@
 
     angular
         .module('baseAngular.i18n')
-        .constant('LOCALES', {
-          'locales': {
-              'es_ES': 'Español',
-              'en_US': 'English'
-          },
-          'preferredLocale': 'es_ES'
-        })
-        .config(['$translateProvider', 'tmhDynamicLocaleProvider', 'LOCALES',
-          function ($translateProvider, tmhDynamicLocaleProvider, LOCALES) {
-            $translateProvider.useStaticFilesLoader({
-              prefix: 'app/resources/locale-',
-              suffix: '.json'
-            })
-              .preferredLanguage(LOCALES.preferredLocale)
-              .useSanitizeValueStrategy('sanitize')
-              .useMissingTranslationHandlerLog();
-            tmhDynamicLocaleProvider.localeLocationPattern("bower_components/angular-i18n/angular-locale_{{locale}}.js");
-          }])
         .factory('i18nService', ['$translate', 'tmhDynamicLocale', 'LOCALES', I18nService]);
 
     function I18nService($translate, tmhDynamicLocale, LOCALES) {
