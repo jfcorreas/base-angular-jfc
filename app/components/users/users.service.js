b47083c863b12ca4998722c4c496fdaf6f2e8ed8
Juanfran Correas <jfcorreas@gmail.com>
2016-03-05 18:46:27 +0100

    UserService doLogin added

--- app/components/users/users.service.js
+++ app/components/users/users.service.js
@@ -11,8 +11,8 @@
     angular
         .module('baseAngular.users')
         .constant('USER_ROLES', {
-            admin: 'admin',
-            guest: 'guest'
+            admin: 'Administrator',
+            guest: 'Guest'
         })
         .constant('AUTH_EVENTS', {
           loginSuccess: 'auth-login-success',
@@ -25,18 +25,53 @@
         .factory('UsersService', ['USER_ROLES', 'AUTH_EVENTS', '_', UsersService]);
 
     function UsersService(USER_ROLES, AUTH_EVENTS, _) {
-      var fakeUsers = [
-        { username: 'guest', password:'guest', userRoles: [USER_ROLES.guest]},
-        { username: 'admin', password:'admin', userRoles: [USER_ROLES.admin]}
-      ];
+      var fakeUsers = {
+        'guest': { password:'guest', userRoles: [USER_ROLES.guest]},
+        'admin': { password:'admin', userRoles: [USER_ROLES.admin]}
+      };
       var currentUser = {};
       var service = {
-        getAuthenticatedUser: authenticatedUser
+        getAuthenticatedUser: authenticatedUser,
+        doLogin: login
       };
       return service;
 
       function authenticatedUser(){
         return currentUser;
       }
+
+      function login(username, password, callback) {
+        var response = {};
+        Object.keys(fakeUsers).some(function(key) {
+          if (key === username && fakeUsers[key].password === password) {
+            response = { success: true };
+            return true;
+          }
+        });
+        if (_.isEmpty(response)) {
+          callback({ success: false, message: "Username or password is incorrect" });
+        } else {
+          callback(response);
+        }
+      }
+    /*  function SetCredentials(username, password) {
+          var authdata = Base64.encode(username + ':' + password);
+
+          $rootScope.globals = {
+              currentUser: {
+                  username: username,
+                  authdata: authdata
+              }
+          };
+
+          $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+          $cookieStore.put('globals', $rootScope.globals);
+      }
+
+      function ClearCredentials() {
+          $rootScope.globals = {};
+          $cookieStore.remove('globals');
+          $http.defaults.headers.common.Authorization = 'Basic';
+      }*/
     }
 })();
--- app/components/users/users.service.spec.js
+++ app/components/users/users.service.spec.js
@@ -12,10 +12,39 @@ describe('Users Service', function() {
     UsersService.should.exist;
   });
 
+  it ('On first load, no user is authenticated', function() {
+    UsersService.getAuthenticatedUser.should.exist;
+    var emptyUser = UsersService.getAuthenticatedUser();
+    emptyUser.should.be.empty;
+  });
+
+  describe('Login', function() {
+    it ('Should authenticate a valid user', function() {
+      UsersService.doLogin('guest','guest', function(response) {
+        response.success.should.be.true;
+      });
+    });
+
+    it ('Should fail with an incorrect user', function() {
+      UsersService.doLogin('falseUser','guest', function(response) {
+        response.success.should.be.false;
+        response.message.should.be.equal('Username or password is incorrect');
+      });
+    });
+
+    it ('Should fail with an incorrect password', function() {
+      UsersService.doLogin('guest','falsePassword', function(response) {
+        response.success.should.be.false;
+        response.message.should.be.equal('Username or password is incorrect');
+      });
+    });
+  });
+
   it ('Should supply the current authenticated user', function() {
     UsersService.getAuthenticatedUser.should.exist;
     var emptyUser = UsersService.getAuthenticatedUser();
     emptyUser.should.be.empty;
     // currentUser.userRoles.length.should.be.above(0);
   });
+
 });
